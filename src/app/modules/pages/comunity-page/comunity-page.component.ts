391e8f0dfeecd204fc2d6a2ba9102c5d08d66b0a
Diego <DiegoViscarra@users.github.com>
2020-03-23 00:59:41 -0400

    Comunidad con bootstrap maso

---
+++ src/app/modules/pages/comunity-page/comunity-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comunity-page',
+  templateUrl: './comunity-page.component.html',
+  styleUrls: ['./comunity-page.component.css']
+})
+export class ComunityPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
